aec17f112e8efa1a8ca53ef7d20edbea0fa2c044
blackcrabb <niyati20002000@gmail.com>
2020-07-10 19:00:43 +0530

    till 1.9

--- src/index.js
+++ src/index.js
@@ -88,6 +88,56 @@ const App = () => {
 }*/
 
 
+ //1.9
+const Statistics = (props) =>{
+
+  if((props.good + props.neutral + props.bad) === 0)
+  {
+    return (
+      <>No feedback given</>
+    )
+  }
+
+  return (
+    <>
+      <br/>
+      good {props.good}
+      <br/>
+      neutral {props.neutral}
+      <br/>
+      bad {props.bad}
+      <br/>
+      all {props.good + props.neutral + props.bad}
+      <br/>
+      average {(props.good*1 + props.neutral*0 + props.bad*-1) / (props.good+props.neutral+props.bad)}
+      <br/>
+      positive {(props.good / (props.good+props.bad+ props.neutral))*100}%
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1> give feedback </h1>
+      <br/>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <br/>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
+    </div>
+  )
+}
+
+
 ReactDOM.render(<App />, document.getElementById('root'))
 
 
